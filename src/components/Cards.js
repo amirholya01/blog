2371e2b8c345def1c1683c2266651c960ede370a
amirholya01 <amirholya01@gmail.com>
2023-10-11 15:49:03 +0200

    complete cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import styles from './Cards.module.css';
 import Card from './Card';
+import java from '../images/java.jpg';
+import js from '../images/js.webp';
+import de from '../images/de.jpg';
+import reacrJS from '../images/react.png';
+
 class Cards extends Component {
     render() {
         return (
-            <div>
-                <Card />
+            <div className={styles.container}>
+                <Card image={java} name="Java Tutorial" cost="Free"/>
+                <Card image={js} name="JavaScript Tutorial" cost="Free"/>
+                <Card image={de} name="Deutsch Lernen" cost="Free"/>
+                <Card image={reacrJS} name="React JS Tutorial" cost="Free"/>
             </div>
         );
     }
